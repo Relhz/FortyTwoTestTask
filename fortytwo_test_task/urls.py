ccbe7a8b172e93d6a7fd5fe66d7d48de407dfaaf
Eugene <yevhenkudrya@gmail.com>
2016-05-22 00:21:41 +0300

    ticket:1 add admin fixtures

--- fortytwo_test_task/urls.py
+++ fortytwo_test_task/urls.py
@@ -7,6 +7,6 @@ admin.autodiscover()
 
 urlpatterns = patterns(
     '',
-    url(r'^', include('hello.urls')),
+    url(r'^$', include('hello.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
