3a596623051f73fc6f9552956a1d1e43b0e34946
Eugene <yevhenkudrya@gmail.com>
2016-05-29 00:03:28 +0300

    modified js

--- apps/hello/static/hello/js/requests_page.js
+++ apps/hello/static/hello/js/requests_page.js
@@ -1,40 +1,4 @@
-$(document).ready(function(){
-
-	// load list of all requests
-	function requests_list(data) {
-
-    	for(i = 0; i < 10; i++){	        
-    		$('.path:eq(' + i + ')').html(data[i].method + 
-        ' ' + data[i].path + ' ; ' + data[i].status_code + 
-        ' ; ' + data[i].date_and_time.slice(0, 16))
-    	}
-    }
-
-    // reset requests amount
-    function reset(){
-	    $('.count').html('')
-		document.title = 'Requests'
-		$.ajax({
-		    url: '/forajax_count_reset/',
-		    type: 'GET', 
-    	})
-    }
-
-    $.ajax({
-        url : "/forajax2/", 
-        type : "GET", 	        
-        success : function(data){ requests_list(data) }
-    });
-
-	// write the amount of requests to a header and a title
-    $.ajax({
-	    url: '/forajax_count/',
-	    type: 'GET', 
-	    success: function(data) {
-	        $('.count').html('('+ data.amount + ')')	  
-		    document.title = '('+ data.amount + ')' + 'Requests'        
-	    },
-	});
+$(document).ready(function(){ 
 
 	// checking for new requests
     setInterval(function(){ 
@@ -42,16 +6,41 @@ $(document).ready(function(){
 	    $.ajax({
 	        url : "/forajax2/", 
 	        type : "GET", 	        
-	        success : function(data){ requests_list(data) }
-	    })	
+	        success : function(data) {
+
+	        	for(i = 0; i < 10; i++){	        
+	        		$('.path:eq(' + i + ')').html(data[i].method + 
+		        ' ' + data[i].path + ' ; ' + data[i].status_code + 
+		        ' ; ' + data[i].date_and_time.slice(0, 16))
+
+	        	}
+	        },
+	    });
+		
 	}, 1000);
 
-    // if user look at this page, reset requests counter
-	$(window).focus(function () {
-    	setTimeout(reset, 3000);
-    })
-    
-	setTimeout(reset, 3000);
+    // reset requests counter when page load
+	setTimeout(function(){ 
+		$('.count').html('')
+		document.title = 'Requests'
+		$.ajax({
+		    url: '/forajax_count_reset/',
+		    type: 'GET', 
+		    success : function(data) {
+		    }
+		});
+	}, 3000);
+
+	// write the amount of requests to the header and the title
+    $.ajax({
+	    url: '/forajax_count/',
+	    type: 'GET', 
+	    success: function(data) {
+	        
+	        $('.count').html('('+ data.amount + ')')	  
+		    document.title = '('+ data.amount + ')' + 'Requests'        
+	    },
+	});
 
 });
 
