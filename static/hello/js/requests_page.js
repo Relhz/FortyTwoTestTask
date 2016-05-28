f88a055c193614f19b4ecd6a8d5e72a4a96f4292
Eugene <yevhenkudrya@gmail.com>
2016-05-28 22:47:23 +0300

    master: collectstatic

--- static/hello/js/requests_page.js
+++ static/hello/js/requests_page.js
@@ -1,37 +1,29 @@
-$(document).ready(function(){ 
+$(document).ready(function(){
 
-	// checking for new requests
-    setInterval(function(){ 
-		
-	    $.ajax({
-	        url : "/forajax2/", 
-	        type : "GET", 	        
-	        success : function(data) {
+	// load list of all requests
+	function requests_list(data) {
 
-	        	for(i = 0; i < 10; i++){	        
-	        		$('.path:eq(' + i + ')').html(data[i].method + 
-		        ' ' + data[i].path + ' ; ' + data[i].status_code + 
-		        ' ; ' + data[i].date_and_time.slice(0, 16))
+    	for(i = 0; i < 10; i++){	        
+    		$('.path:eq(' + i + ')').html(data[i].method + 
+        ' ' + data[i].path + ' ; ' + data[i].status_code + 
+        ' ; ' + data[i].date_and_time.slice(0, 16))
+    	}
+    }
 
-	        	}
-	        },
-	    });
-		
-	}, 1000);
+    // reset requests amount
+    function reset(){
+	    $('.count').html('')
+		document.title = 'Requests'
+		$.ajax({
+		    url: '/forajax_count_reset/',
+		    type: 'GET', 
+    	})
+    }
 
-    // if user look at this page, reset requests counter
-	$(window).focus(function () {
-    	setTimeout(function(){ 
-    		$('.count').html('')
-    		document.title = 'Requests'
-			$.ajax({
-			    url: '/forajax_count_reset/',
-			    type: 'GET', 
-			    success : function(data) {
-			    }
-			});
-    	}, 3000);
-        
+    $.ajax({
+        url : "/forajax2/", 
+        type : "GET", 	        
+        success : function(data){ requests_list(data) }
     });
 
 	// write the amount of requests to the header and the title
@@ -39,11 +31,39 @@ $(document).ready(function(){
 	    url: '/forajax_count/',
 	    type: 'GET', 
 	    success: function(data) {
-	        
 	        $('.count').html('('+ data.amount + ')')	  
 		    document.title = '('+ data.amount + ')' + 'Requests'        
 	    },
 	});
 
+	// checking for new requests
+    setInterval(function(){ 
+		
+	    $.ajax({
+	        url : "/forajax2/", 
+	        type : "GET", 	        
+	        success : function(data){ requests_list(data) }
+	    })	
+	}, 1000);
+
+    // if user look at this page, reset requests counter
+	$(window).focus(function () {
+    	setTimeout(reset, 3000);
+    })
+    
+	setTimeout(reset, 3000);
+
+	$('.path').bind("DOMSubtreeModified",function(){
+
+		$.ajax({
+		    url: '/forajax_count/',
+		    type: 'GET', 
+		    success : function(data) {
+		    	$('.count').html('('+ data.amount + ')')	  
+	    		document.title = '('+ data.amount + ')' + 'Requests'
+
+		    }
+		});       
+    });
 });
 
