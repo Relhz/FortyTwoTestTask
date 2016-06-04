8ed3bc915cceac76069bed07f1c65ab7a1a6b69e
Eugene <yevhenkudrya@gmail.com>
2016-06-04 13:45:13 +0300

    master: fix static

--- static/hello/js/requests_page.js
+++ static/hello/js/requests_page.js
@@ -1,21 +1,30 @@
 $(document).ready(function(){ 
 
 	// update requests list
+	var old = $('.c:eq(' + 0 + ')').html()
     setInterval(function(){
 	
 	    $.ajax({
-	        url : "/forajax2/", 
+	        url : "/forajax/", 
 	        type : "GET", 
 	        success : function(data) {
 
-	        	for(i = 0; i < 10; i++){
-        			
-		        		$('.path:eq(' + i + ')').html(data[i].method + 
-			        ' ' + data[i].path + ' ; ' + data[i].status_code + 
-			        ' ; ' + data[i].date_and_time.slice(0, 16) + 
-			        '<span class="c" style="display: none">' + data[i].amount 
-			        + '</span>')
-
+		        var current = data[0].amount
+		        new_requests = current - old
+		        if(new_requests > 0){
+		        	for(i = 0; i < new_requests; i++){
+			        	$('.path:eq(' + ($(".path").length - 1) + ')').remove()
+			        	
+			        	$('.path:eq(0)').before(
+			        		'<p class="path">' + data[i].method + 
+					        ' ' + data[i].path + ' ; ' + data[i].status_code +
+					        ' ; ' + data[i].date_and_time.slice(0, 16) + 
+					        '<span class="c" style="display: none">' + 
+					        data[i].amount + '</span></p>'
+		        		)
+				        
+				    old = $('.c:eq(' + 0 + ')').html()
+		        	}
 	        	}
 	        },
 	    });
@@ -33,12 +42,11 @@ $(document).ready(function(){
 	var past = $('.c:eq(' + 0 + ')').html()
 	interval = setInterval(function(){
 	    $.ajax({
-		    url: '/forajax_count/',
+		    url: '/forajax/',
 		    type: 'GET', 
 		    success: function(data) {
 		        
-		        var current = data.amount
-		        console.log(past, current)
+		        var current = data[0].amount
 		        if((current - past) > 0){
 		        	$('.count').html('('+ (current - past) + ')')	  
 			    	document.title = '(' + (current - past) + ')Requests'
@@ -50,7 +58,7 @@ $(document).ready(function(){
     // reset requests counter when user view page
 	$(window).focus(function () {
 		clearInterval(interval)
-    	setTimeout(function(){ 
+		setTimeout(function(){ 
     		$('.count').html('')
     		document.title = 'Requests'
     	}, 2000);
@@ -64,16 +72,15 @@ $(document).ready(function(){
 		var past = $('.c:eq(' + 0 + ')').html()
 		interval = setInterval(function(){
 		    $.ajax({
-			    url: '/forajax_count/',
+			    url: '/forajax/',
 			    type: 'GET', 
 			    success: function(data) {
 
-			        var current = data.amount
+			        var current = data[0].amount
 			        if((current - past) > 0){
 			        	$('.count').html('('+ (current - past) + ')')	  
 				    	document.title = '(' + (current - past) + ')Requests'
 			        }
-
 			    },
 			});
 		}, 1000);
