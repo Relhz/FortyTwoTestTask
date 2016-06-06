63642301151fea1d582fc8c2e93f575e2cd646de
Eugene <yevhenkudrya@gmail.com>
2016-06-06 18:08:36 +0300

    ticket:1 temlate, view

--- apps/hello/views.py
+++ apps/hello/views.py
@@ -6,13 +6,6 @@ from django.core.exceptions import MultipleObjectsReturned
 
 def main(request):
 
-    message = ''
-    info = False
-
-    if Info.objects.all():
-        info = Info.objects.all().first()
-    else:
-        message = 'Database is empty'
+    info = Info.objects.all().first()
     
-    return render(request, 'hello/main.html', {'info': info,
-                 'message': message})
+    return render(request, 'hello/main.html', {'info': info})
