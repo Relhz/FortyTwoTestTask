7916de10ad69ed2fca1b98b0e2a06cf1cf950867
Eugene <yevhenkudrya@gmail.com>
2016-06-07 14:27:54 +0300

    master: add migr

---
+++ apps/hello/migrations/0003_auto__alter_field_date_of_birst.py
@@ -0,0 +1,67 @@
+# -*- coding: utf-8 -*-
+from south.utils import datetime_utils as datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Deleting field 'Info.date_of_birst'
+        db.delete_column(u'hello_info', 'date_of_birst')
+
+        # Adding field 'Info.date_of_birth'
+        db.add_column(u'hello_info', 'date_of_birth',
+                      self.gf('django.db.models.fields.DateField')(null=True, blank=True),
+                      keep_default=False)
+
+
+        # Changing field 'Info.last_name'
+        db.alter_column(u'hello_info', 'last_name', self.gf('django.db.models.fields.CharField')(max_length=20))
+
+        # Changing field 'Info.name'
+        db.alter_column(u'hello_info', 'name', self.gf('django.db.models.fields.CharField')(max_length=20, null=True))
+
+    def backwards(self, orm):
+        # Adding field 'Info.date_of_birst'
+        db.add_column(u'hello_info', 'date_of_birst',
+                      self.gf('django.db.models.fields.DateField')(null=True, blank=True),
+                      keep_default=False)
+
+        # Deleting field 'Info.date_of_birth'
+        db.delete_column(u'hello_info', 'date_of_birth')
+
+
+        # Changing field 'Info.last_name'
+        db.alter_column(u'hello_info', 'last_name', self.gf('django.db.models.fields.CharField')(max_length=10))
+
+        # Changing field 'Info.name'
+        db.alter_column(u'hello_info', 'name', self.gf('django.db.models.fields.CharField')(max_length=10, null=True))
+
+    models = {
+        u'hello.info': {
+            'Meta': {'object_name': 'Info'},
+            'bio': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'contacts': ('django.db.models.fields.CharField', [], {'max_length': '50', 'null': 'True', 'blank': 'True'}),
+            'date_of_birth': ('django.db.models.fields.DateField', [], {'null': 'True', 'blank': 'True'}),
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '254', 'null': 'True', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'jabber': ('django.db.models.fields.CharField', [], {'max_length': '50', 'null': 'True', 'blank': 'True'}),
+            'last_name': ('django.db.models.fields.CharField', [], {'default': "u'Surname'", 'max_length': '20'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '20', 'null': 'True', 'blank': 'True'}),
+            'other_contacts': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'photo': ('django.db.models.fields.files.ImageField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'}),
+            'skype': ('django.db.models.fields.CharField', [], {'max_length': '50', 'null': 'True', 'blank': 'True'})
+        },
+        u'hello.requests': {
+            'Meta': {'object_name': 'Requests'},
+            'date_and_time': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime(2016, 6, 5, 0, 0)'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'method': ('django.db.models.fields.CharField', [], {'default': "u'Post'", 'max_length': '10'}),
+            'path': ('django.db.models.fields.CharField', [], {'default': "u'path'", 'max_length': '300'}),
+            'status_code': ('django.db.models.fields.CharField', [], {'default': "u'200'", 'max_length': '10'})
+        }
+    }
+
+    complete_apps = ['hello']
