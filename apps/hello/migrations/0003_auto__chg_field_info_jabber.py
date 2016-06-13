df6f5020e595cb17d92fe6d826c9b30e5f7d1cb5
Eugene <yevhenkudrya@gmail.com>
2016-06-13 13:38:37 +0300

    ticket:1 validators

---
+++ apps/hello/migrations/0003_auto__chg_field_info_jabber.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+from south.utils import datetime_utils as datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+
+        # Changing field 'Info.jabber'
+        db.alter_column(u'hello_info', 'jabber', self.gf('django.db.models.fields.CharField')(max_length=50, null=True))
+
+    def backwards(self, orm):
+
+        # Changing field 'Info.jabber'
+        db.alter_column(u'hello_info', 'jabber', self.gf('django.db.models.fields.EmailField')(max_length=254, null=True))
+
+    models = {
+        u'hello.info': {
+            'Meta': {'object_name': 'Info'},
+            'bio': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'contacts': ('django.db.models.fields.CharField', [], {'max_length': '50', 'null': 'True', 'blank': 'True'}),
+            'date_of_birth': ('django.db.models.fields.DateField', [], {'null': 'True', 'blank': 'True'}),
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '254', 'null': 'True', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'jabber': ('django.db.models.fields.CharField', [], {'max_length': '50', 'null': 'True', 'blank': 'True'}),
+            'last_name': ('django.db.models.fields.CharField', [], {'default': "u'Surname'", 'max_length': '20'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '20', 'null': 'True', 'blank': 'True'}),
+            'other_contacts': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'skype': ('django.db.models.fields.CharField', [], {'max_length': '50', 'null': 'True', 'blank': 'True'})
+        }
+    }
+
+    complete_apps = ['hello']
