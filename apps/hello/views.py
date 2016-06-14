cc9d912f7eb9bfef26a6cd2f1691092aedb20eb6
Eugene <yevhenkudrya@gmail.com>
2016-06-15 00:24:42 +0300

    ticket:1 logging

--- apps/hello/views.py
+++ apps/hello/views.py
@@ -1,10 +1,14 @@
 # -*- coding: utf-8 -*-
 from django.shortcuts import render
 from models import Info
+import logging
+log = logging.getLogger('django')
 
 
 def main(request):
 
     info = Info.objects.first()
+
+    log.warn('variables: ' + str(info))
 
     return render(request, 'hello/main.html', {'info': info})
