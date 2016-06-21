036692bc7518c8b2f6a3d39cfeeb314a87cbf5e3
Eugene <yevhenkudrya@gmail.com>
2016-06-21 17:48:14 +0300

    ticket:5 loading state

--- apps/hello/static/hello/js/edit_page.js
+++ apps/hello/static/hello/js/edit_page.js
@@ -23,6 +23,25 @@ $(document).ready(function(){
         readURL(this);
     });
 
-    $('#sendpost').ajaxForm(); 
+    $('#sendpost').ajaxForm({
+        beforeSend: function() {
+            $('#status').empty();
+            var percentVal = '0%';
+            $('.bar').width(percentVal);
+            $('.percent').html(percentVal);
+        },
+        uploadProgress: function(event, position, total, percentComplete) {
+            var percentVal = percentComplete + '%';
+            $('.bar').width(percentVal);
+            $('.percent').html(percentVal);
+        },
+        complete: function(xhr) {
+            setTimeout(function(){
+            	$('.bar').hide()
+            	$('.percent').hide()
+            	$('#status').hide()
+            }, 3000)
+        }
+    }); 
 
 })
