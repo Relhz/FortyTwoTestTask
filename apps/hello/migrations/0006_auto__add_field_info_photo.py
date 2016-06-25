1287309dce651b8ab0b1506dbdb405dddc62b901
Eugene <yevhenkudrya@gmail.com>
2016-06-25 16:37:35 +0300

    master: fix migr

--- apps/hello/migrations/0006_auto__add_field_info_photo.py
+++ apps/hello/migrations/0006_auto__add_field_info_photo.py
@@ -7,16 +7,18 @@ from django.db import models
 
 class Migration(SchemaMigration):
 
+    needed_by = (
+        ("hello", "0008_auto_add_requests"),
+    )
+
     def forwards(self, orm):
         # Adding field 'Info.photo'
-        db.add_column(u'hello_info', 'photo',
-                      self.gf('django.db.models.fields.files.ImageField')(max_length=100, null=True, blank=True),
-                      keep_default=False)
+        pass
 
 
     def backwards(self, orm):
         # Deleting field 'Info.photo'
-        db.delete_column(u'hello_info', 'photo')
+        pass
 
 
     models = {
@@ -27,20 +29,19 @@ class Migration(SchemaMigration):
             'date_of_birth': ('django.db.models.fields.DateField', [], {'null': 'True', 'blank': 'True'}),
             'email': ('django.db.models.fields.EmailField', [], {'max_length': '254', 'null': 'True', 'blank': 'True'}),
             u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'jabber': ('django.db.models.fields.CharField', [], {'max_length': '50', 'null': 'True', 'blank': 'True'}),
+            'jabber': ('django.db.models.fields.EmailField', [], {'max_length': '50', 'null': 'True', 'blank': 'True'}),
             'last_name': ('django.db.models.fields.CharField', [], {'default': "u'Surname'", 'max_length': '20'}),
             'name': ('django.db.models.fields.CharField', [], {'max_length': '20', 'null': 'True', 'blank': 'True'}),
             'other_contacts': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
-            'photo': ('django.db.models.fields.files.ImageField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'}),
-            'skype': ('django.db.models.fields.CharField', [], {'max_length': '50', 'null': 'True', 'blank': 'True'})
+            'skype': ('django.db.models.fields.CharField', [], {'max_length': '50', 'null': 'True', 'blank': 'True'}),
+            'photo': ('django.db.models.fields.files.ImageField', [], {'null': 'True', 'blank': 'True'})
         },
         u'hello.requests': {
             'Meta': {'object_name': 'Requests'},
-            'date_and_time': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime(2016, 6, 5, 0, 0)'}),
+            'requests_date_time': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime(2016, 6, 7, 0, 0)'}),
             u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'method': ('django.db.models.fields.CharField', [], {'default': "u'Post'", 'max_length': '10'}),
             'path': ('django.db.models.fields.CharField', [], {'default': "u'path'", 'max_length': '300'}),
-            'status_code': ('django.db.models.fields.CharField', [], {'default': "u'200'", 'max_length': '10'})
         }
     }
 
