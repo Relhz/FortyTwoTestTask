2f77a32b4af14ceb5665140aedbab78270ecadf1
Eugene <yevhenkudrya@gmail.com>
2016-06-25 19:34:30 +0300

    master: fix migr4

--- apps/hello/migrations/0008_auto_add_requests.py
+++ apps/hello/migrations/0008_auto_add_requests.py
@@ -7,6 +7,10 @@ from django.db import models
 
 class Migration(SchemaMigration):
 
+    depends_on = (
+        ("hello", "0006_auto__add_field_info_photo"),
+    )
+
     def forwards(self, orm):
 
         # Adding model 'Requests'
