68be2f92397c5e1c06a5e6fa13829e2bd44fa960
Eugene <yevhenkudrya@gmail.com>
2016-06-25 16:13:16 +0300

    ticket:8 fix flake8

--- apps/hello/__init__.py
+++ apps/hello/__init__.py
@@ -1,1 +1,4 @@
 import signals
+
+
+__all__ = ['signals']
--- apps/hello/tests/test_signals.py
+++ apps/hello/tests/test_signals.py
@@ -9,28 +9,28 @@ class SignalsTest(TestCase):
 
     def test_signal_create(self):
 
-    	''' test append the record into db if new object created '''
+        ''' test append the record into db if new object created '''
 
-    	Info.objects.create(last_name='nEwAweSoMESUrname')
-    	self.assertEqual(ModelsAction.objects.last().modelname, Info.__name__)
-    	self.assertEqual(ModelsAction.objects.last().action, 'Create')
+        Info.objects.create(last_name='nEwAweSoMESUrname')
+        self.assertEqual(ModelsAction.objects.last().modelname, Info.__name__)
+        self.assertEqual(ModelsAction.objects.last().action, 'Create')
 
     def test_signal_edit(self):
 
-    	''' test append the record into db if object edited '''
+        ''' test append the record into db if object edited '''
 
-    	Info.objects.create(last_name='nEwAweSoMESUrname')
-    	info = Info.objects.last()
-    	info.skype = 'lolskype'
-    	info.save()
-    	self.assertEqual(ModelsAction.objects.last().modelname, Info.__name__)
-    	self.assertEqual(ModelsAction.objects.last().action, 'Edit')
+        Info.objects.create(last_name='nEwAweSoMESUrname')
+        info = Info.objects.last()
+        info.skype = 'lolskype'
+        info.save()
+        self.assertEqual(ModelsAction.objects.last().modelname, Info.__name__)
+        self.assertEqual(ModelsAction.objects.last().action, 'Edit')
 
     def test_signal_delete(self):
 
-    	''' test append the record into db if object deleted '''
+        ''' test append the record into db if object deleted '''
 
-    	Info.objects.create(last_name='nEwAweSoMESUrname')
-    	Info.objects.last().delete()
-    	self.assertEqual(ModelsAction.objects.last().modelname, Info.__name__)
-    	self.assertEqual(ModelsAction.objects.last().action, 'Delete')
+        Info.objects.create(last_name='nEwAweSoMESUrname')
+        Info.objects.last().delete()
+        self.assertEqual(ModelsAction.objects.last().modelname, Info.__name__)
+        self.assertEqual(ModelsAction.objects.last().action, 'Delete')
