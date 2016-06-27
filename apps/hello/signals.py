5ff8fd5863a1f899a2fbc63d593ef2314755757b
Eugene <yevhenkudrya@gmail.com>
2016-06-27 18:33:20 +0300

    ticket:8 ignored models in settings

--- apps/hello/signals.py
+++ apps/hello/signals.py
@@ -2,27 +2,25 @@ from apps.hello.models import ModelsAction
 from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
 import logging
+from fortytwo_test_task.settings.common import IGNORED_SIGNALS
 
 
 logger = logging.getLogger(__name__)
 
 
-ignored = ['ModelsAction']
-
-
 @receiver(post_save, dispatch_uid='uid')
 def post_save_signal(sender, created, **kwargs):
     logger.debug('sender >>>>>>>>>> ' + str(sender))
-    if created and sender.__name__ not in ignored:
+    if created and sender.__name__ not in IGNORED_SIGNALS:
         ModelsAction.objects.create(modelname=sender.__name__,
                                     action='Create')
-    elif sender.__name__ not in ignored:
+    elif sender.__name__ not in IGNORED_SIGNALS:
         ModelsAction.objects.create(modelname=sender.__name__, action='Edit')
 
 
 @receiver(post_delete, dispatch_uid='uid')
 def post_delete_signal(sender, **kwargs):
     logger.debug('sender >>>>>>>>>> ' + str(sender))
-    if sender.__name__ not in ignored:
+    if sender.__name__ not in IGNORED_SIGNALS:
         ModelsAction.objects.create(modelname=sender.__name__,
                                     action='Delete')
