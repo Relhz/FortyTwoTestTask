ce161c7dadcf62aa39cf99e667d399ee4e4323f3
Eugene <yevhenkudrya@gmail.com>
2016-06-27 19:15:18 +0300

    ticket:8 tag

--- apps/hello/templatetags/edit_obj.py
+++ apps/hello/templatetags/edit_obj.py
@@ -7,5 +7,6 @@ register = template.Library()
 
 @register.simple_tag
 def into_admin(obj):
-
+    if not hasattr(obj, '_meta'):
+        return reverse('main')
     return reverse('admin:%s_change' % (obj._meta.db_table), args=(obj.id,))
